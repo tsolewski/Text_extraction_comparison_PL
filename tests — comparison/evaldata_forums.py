85494e8d534250850dea1dd85e6429031c1971b0
tsolewski <57156100+tsolewski@users.noreply.github.com>
2021-06-02 12:08:20 +0200

    Add files via upload

---
+++ tests — comparison/evaldata_forums.py
@@ -0,0 +1,116 @@
+EVAL_PAGES = {
+'https://forum.gazeta.pl/forum/w,29055,171560136,171560136,Potrzebuje_pomocy_w_sprawach_komputrowych.html': {
+    'file': 'Potrzebuje pomocy w sprawach komputrowych - _+_- 60_ jesteśmy ,,,,,,,, - Forum dyskusyjne _ Gazeta.pl.html',
+    'with': ['od kogos, kto', 'Kółeczko od czego?', 'wiec je przetestuje', 'po czyszczeniu na nowo', 'Wszystko ma swoje', 'komputer na amen'],
+    'without': ['Re: Potrzebuje', 'wikul', 'Obserwuj wątek', 'napisał:', 'krakkrakusek1au', '15.04.21'],
+},
+'https://wizaz.pl/forum/showthread.php?p=88698973#post88698973': {
+    'file': 'PEPCO cz. III - Strona 110 - Wizaz.pl.html',
+    'with': ['U mnie w Pepco', 'nową dostawą...', 'wrzucały strasznie duże', 'Nie widziałam golarek', 'muszę się wybrać', 'i to sporo'],
+    'without': ['PEPCO cz. III', 'Wysłane z', 'Zakorzenienie', 'Cytat:', 'Napisane przez', 'sierpień 2017'],
+},
+'https://vitalia.pl/forum1,1043163,0_Czy-szybkie-przytycie-oznacza-szybkie-schudniecie.html': {
+    'file': 'Czy szybkie przytycie oznacza szybkie schudnięcie_ - strona 1 _ Vitalia.html',
+    'with': ['Zawsze mówi się', 'sobie na więcej', 'No właśnie :c', 'zrzuciłam to wszystko', 'nie byłoby ludzi', 'bardzo niesprawiedliwe, bardzo'],
+    'without': ['Wczoraj', 'Udostępnij post', 'Ursa_Minor', 'napisał(a):', 'Dzisiaj', '#6'],
+},
+'https://www.dogomania.com/forum/topic/138323-preparaty-na-kleszcze/': {
+    'file': 'preparaty na kleszcze - Kosmetyki - Dogomania.html',
+    'with': ['Jak radzicie sobie z', 'tym w dziale', 'krople u weterynarza', 'typ nietabletkowy.', 'Kiltix i Foresto', 'U mnie idealnie'],
+    'without': ['Quote', 'Posted', '1 month later...', 'Members', 'You can post'],
+},
+'https://www.sfd.pl/Pytanie_o_plan_treningowy_%C5%82awka%2Bhantle%2Bsztanga_%C5%82amana_dla_biegacza-t1211239.html': {
+    'file': 'Pytanie o plan treningowy (ławka+hantle+sztanga łamana) dla biegacza - Forum SFD.html',
+    'with': ['Zabieram się za', 'Będę wdzięczny za pomoc', 'Jeżeli weźmiesz trenera', 'Jem dużo, bo', 'Edit. Jak coś to', 'Ja w miesiąc zapinam'],
+    'without': ['Wszystko ma swój cel.', 'napisał(a):', 'April', 'temat działu:', 'łamana , biegacza', 'Witaj, przygotowaliśmy kilka'],
+},
+'https://www.forumowisko.pl/threads/szczepionki-juz-w-styczniu-zaszczepicie-sie.223604/': {
+    'file': 'Szczepionki już w styczniu - zaszczepicie się_ _ Forumowisko.pl.html',
+    'with': ['Jak w tytule. Już ', 'już nie będzie trzeba mieć', 'Prawdopodobnie będzie Amerykańska', 'Karmiącej kobiecie można', 'rzeczywiste nopy.', 'ludzi, którzy wiedzą lepiej'],
+    'without': ['Grudzień 2020', 'Bywalec', '#1', 'napisał:', 'Michał Wawer', 'Rząd brytyjski opublikował'],
+},
+'https://www.forumowisko.pl/threads/sebix-vs-policja-w-jeleniej-gorze-dlaczego-bronimy-patusow.223531/': {
+    'file': 'Sebix vs. Policja w Jeleniej Górze - Dlaczego bronimy patusów_ _ Forumowisko.pl.html',
+    'with': ['Widziałem filmiki z interwencji', 'Za to opowiada promowanie', 'I bicie za to', 'inna rzeczywistość!'],
+    'without': ['napisał:' , 'wrzesień 2020', 'Bywalec', '#1', 'www.wykop.pl', 'Aktualności z Polski i świata '],
+},
+'http://www.hdtv.com.pl/forum/newsy/82099-jaki-telewizor-wybrac-przygotuj-odpowiedzi-na-ponizsze-pytania.html': {
+    'file': 'Jaki telewizor wybrać _ Przygotuj odpowiedzi na poniższe pytania. - HDforum.html',
+    'with': ['Codziennie setki a może', 'bądź większość potrzeb.', 'Jak daleko siedzimy od ekranu telewizora?', 'często z aplikacji internetowych'],
+    'without': ['Redakcja HDTV.com.pl', 'Bądź wspierającym Forum', 'Ostatnio edytowane', 'kratki na elewacji', 'odpowiedzi na poniższe'],
+},
+'http://www.hdtv.com.pl/forum/newsy/120554-tabela-odleglosci-dla-4k.html': {
+    'file': 'Tabela odległości dla 4K. - HDforum.html',
+    'with': ['Kalkulator odległości poniżej', 'Nie dajcie się zwariować', 'Sorry a kto wariuje?', 'Uwierz mi że komp', 'Grubo powyżej 5000zł'],
+    'without': ['Bądź wspierającym Forum', 'Ostatnio edytowane', 'Moja galeria', 'TV: Samsung UHD'],
+},
+'https://www.forum-bankowe.pl/tematy3/bank-z-czekami-vt11015.htm': {
+    'file': 'Bank z czekami - Forum-Bankowe.pl.html',
+    'with': ['jakimś polskim banku mogę', 'Zapomnij o czekach, już', 'jeszcze tam mają czeki.'],
+    'without': ['napisał(a):', 'Wyświetl posty nie', 'Dołączył:', 'Bank z czekami'],
+},
+'http://forum.ebiznesy.pl/anonimowy/piwko-kawka-hebratka-w-krakowie-t1006.html': {
+    'file': 'Piwko,Kawka,Hebratka w Krakowie _ Wizja e-biznesu • Forum e-biznes e-marketing pozycjonowanie usability.html',
+    'with': ['Wiem że jest kilka osób z Krakowa', 'Ja chcę mieszkać w', 'nie było chętnych ? :>', 'kogoś zrekrutować...', 'Kobieta może jakaś'],
+    'without': ['Sty 2010', 'przez Marcin Godlewski', 'Piwko,Kawka,Hebratka w Krakowie', 'Wyświetl posty'],
+},
+'https://www.gry-online.pl/S043.asp?ID=15642604&N=1': {
+    'file': 'Gra Siege Survival Gloria Victis _ PC 19.05.2021 - Forum _ GRYOnline.pl.html',
+    'with': ['Wygląda jak takie średniowieczne', 'odnośnie tej produkcji...', 'fajne, ale w Early Access.'],
+    'without': ['Gra Siege Survival: Gloria', 'Pretorianin', 'Copyright', '01.06.2021', 'BongMan', '50'],
+},
+'https://www.elektroda.pl/rtvforum/topic3808862.html#19462164': {
+    'file': 'Stale przepalające się świetlówki LED po przesznurowaniu.html',
+    'with': ['CorePro o mocy 20W.', 'Po jakim czasie się', 'Przez miesiąc potrafi', 'Niestety nie ma danych', 'gdzie minus jak dobrze myślę'],
+    'without': ['New topic', 'Zabezpieczenia Internetu', 'Topic author', 'wrote:'],
+},
+'https://www.elektroda.pl/rtvforum/topic3808034.html#19462159': {
+    'file': 'Konwerter monoblock, antena na Astre - elektroda.pl.html',
+    'with': ['pozornie tego typu tematów', 'Inverto albo Opticum.', 'odnośnie parametru "a/h".', 'Zatem ustawię tak, by', 'Astra to odwrotnie.'],
+    'without': ['Konwerter monoblock, antena na Astre', 'See also:', 'Helpful post?', 'Level', 'Similar topics to'],
+},
+'https://forum.pclab.pl/topic/1316533-mysz-do-60z%C5%82/': {
+    'file': 'Mysz do 60zł - Urządzenia wskazujące - Forum PCLab.pl.html',
+    'with': ['Szukam myszy przewodowej do', 'od 50 zł. Co sądzisz?', 'nieźle trzyma baterię;', 'że poziom forum maleje.'],
+    'without': ['dodany przez', 'Edytowane', 'Maja', 'Jeśli chcesz dodać', 'Jedynie zarejestrowani'],
+},
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# 'https://www.msn.com/en-gb/news/uknews/uk-university-student-halls-too-full-to-be-safe-experts-warn/ar-BB19DUqK': {
+#     'file': 'msn.com.university.html',  # stays empty until download
+#     'author': 'Anna Fazackerley',
+#     'title': 'UK university student halls too full to be safe, experts warn ',
+#     'date': '2020-10-02',
+#     'description': 'As increasing numbers of UK universities wrestle with Covid-19 outbreaks, experts are warning that student halls are too full to be safe.&amp;nbsp;',  # in HTML source
+#     'categories': [''],
+#     'tags': [''],
+#     'with': ['s increasing numbers of UK universities wrestle with', 'seven days. In Northern Ireland, call your GP.', 'Some UK universities, including Cambridge, Imperial College London'],  # 3 segments
+#     'without': ['powered by Microsoft News', 'Sign in', 'Commenting is not currently available, but don’t worry'],  # 3 segments
+#     'comments': [''],  # 0 or 3 segments
+#     'license': '',  # if CC-...
+#     'region': 'UK',  # if obvious: DE, CH, AT
+# },
+}
---
+++ tests — comparison/evaldata_news.py
@@ -0,0 +1,151 @@
+EVAL_PAGES = {
+    'https://klubjagiellonski.pl/2021/04/22/nowa-zelandia-platny-urlop-dla-rodzicow-poronionego-dziecka-jak-to-wyglada-w-polsce/': {
+    'file': 'Nowa Zelandia_ płatny urlop dla rodziców poronionego dziecka. Jak to wygląda w Polsce_ — Klub Jagielloński.html',
+    'with': ['Trzy dni', 'na opłakanie straty.', 'w życie w kwietniu.', 'Ustawa Holidays', 'podsumowuje publicysta.'],
+    'without': ['korzystamy z cookies.', 'ZAMKNIJ', 'Czytasz właśnie nowy', 'przeczytanie zajmie', 'Krótko', 'Twój email'],
+},
+    'https://liberte.pl/koncesjonowana-opozycja/': {
+    'file': 'Koncesjonowana opozycja -Leszek Jażdżewski - Liberté!.html',
+    'with': ['Pakt lewicy z PiS', 'jest samo w sobie czymś', 'Podsumujmy. Lewica', 'na salonach władzy.'],
+    'without': ['Fundusz Odbudowy, lewica, PiS', 'Inne artykuły autora', 'Polityka prywatności', 'kwietnia 2021'],
+},
+'https://www.dwutygodnik.com/artykul/9491-winda-w-chmurach.html': {
+    'file': 'Winda w chmurach _ Obyczaje _ Dwutygodnik _ Dwutygodnik.html',
+    'with': ['A co,', 'Ludzie ci', 'Aleksandra Zbroja przedstawia się', 'Aleksandra Zbroja uruchamia', 'Ważne, że w ogóle wyjął', 'Mireczka, i bez niego.'],
+    'without': ['/ obyczaje', 'Airman 1st Class Ariel Leighty', '11 minut czytania', 'Tweetnij', 'Historyczka nowoczesnej kultury polskiej' , 'IWONA KURZ'],
+},
+'https://publica.pl/teksty/rafalska-finowie-odkrywaja-wino-68379.html': {
+    'file': 'Finowie odkrywają wino _ Res Publica Nowa.html',
+    'with': ['Nie mogąc konkurować', 'W Finlandii obowiązuje', 'koncepcję „zero waste”'],
+    'without': ['Dominika Rafalska', 'marca 2021', 'doktor nauk humanistycznych', 'Rozmowa z dr nauk', 'Wino lepsze od'],
+},
+'https://spidersweb.pl/2021/05/windows-10-flash.html': {
+    'file': 'Windows 10 od lipca przestanie obsługiwać Adobe Flash.html',
+    'with': ['Adobe Flash – co by', 'W październiku na', 'wspomniane cyberbezpieczeństwo.', 'nigdy go nie obsługiwały.'],
+    'without': ['NEWS', 'TAGI', 'UDOSTĘPNIJ', 'PRZECZYTAJ NASTĘPNY TEKST'],
+},
+'https://spidersweb.pl/plus/2021/04/influencerzy-celebryci-covid-pandemia-zanzibar-kwarantanna': {
+    'file': 'Polscy pato-influencerzy w akcji. Obchodzą zakazy i przesyłają „trochę słońca_ z egzotycznych plaż.html',
+    'with': ['Sonia Bohosiewicz ma 335', 'Rok temu wrzucali', 'popularnymi #stayhome i #staysafe', 'Julia Wieniawa ma 1,9', 'większość komentarzy była negatywna.'],
+    'without': ['ANNA NICZ', 'W zeszłym roku influencerzy', 'Rajskie plaże i brak', 'Jakie biedne życie trzeba', 'Zdjęcie tytułowe:', '******'],
+},
+'https://www.polityka.pl/tygodnikpolityka/swiat/2116825,1,krolowa-elzbieta-wyplywa-w-swiat-sygnal-dla-chin-i-rosji.read': {
+    'file': '„Królowa Elżbieta” wypływa w świat. Sygnał dla Chin i Rosji - Polityka.pl.html',
+    'with': ['Pierwszy rejs HMS', 'Bandera podąża za handlem', 'zaważyła na jego trasie.'],
+    'without': ['Czytaj też', 'Były dowódca wojsk USA', 'Andrew', 'MINUT CZYTANIA', 'starszy analityk'],
+},
+'https://www.rp.pl/Polityka/305049888-Unijne-fundusze-coraz-blizej-Sejm-zaglosowal-za.html': {
+    'file': 'Unijne fundusze coraz bliżej. Sejm zagłosował _za_ - Polityka - rp.pl.html',
+    'with': ['To jest moment fundamentalny', 'Ostatecznie 290 posłów', 'Ciężar dyskusji przeniesie', 'Na lepszą konkurencyjność gospodarki'],
+    'without': ['Licencja na publikację', 'Dowiedz się więcej', 'PAP/ Rafał Guz', 'Po głosowaniach w', 'Michał Kolanko', 'Publikacja:'],
+},
+'https://forsal.pl/gospodarka/pkb/artykuly/8150873,ile-jest-warta-godzina-pracy.html': {
+    'file': 'Ile jest warta godzina pracy_ W tych krajach praca popłaca - Forsal.pl.html',
+    'with': ['Portal Statista powołuje', '54,4 dolara.', 'Oto kraje, w'],
+    'without': ['praca / ShutterStock', 'Ten tekst przeczytasz', 'statista.com', 'Źródło:'],
+},
+'https://wiadomosci.dziennik.pl/polityka/artykuly/8156148,dezubekizacja-trybunal-konstytucyjny-wyrok-termin.html': {
+    'file': 'Dezubekizacja – reaktywacja. Czyli daty mają znaczenie - Dziennik.pl.html',
+    'with': ['Historia dezubekizacji przed', 'Pod koniec stycznia ', '12 maja. Tego', 'Skarżący powołali się', 'sądy powszechne.'],
+    'without': ['Ten tekst przeczytasz', 'Trybunał Konstytucyjny / Shutterstock', 'REKLAMA', 'CZYTAJ WIĘCEJ W', 'Materiał chroniony prawem', 'Źródło Dziennik Gazeta Prawna'],
+},
+'https://dorzeczy.pl/ekonomia/183131/eurostat-polska-z-najnizszym-bezrobociem-w-calej-ue.html': {
+    'file': 'Eurostat_ Polska z najniższym bezrobociem w całej UE.html',
+    'with': ['Wyrównana sezonowo stopa', 'Liczba bezrobotnych w Polsce', 'miesiąc wcześniej – podano.', 'Holandia (3,5 proc.).', 'Eurostat mierzy zharmonizowaną', 'najniższa w UE – podał Eurostat.'],
+    'without': ['Praca w magazynie', 'Źródło: ISBnews', 'NAJNOWSZE'],
+},
+'https://www.portalspozywczy.pl/handel/wiadomosci/eurocash-rusza-z-innowacyjna-platforma-handlu,198264.html': {
+    'file': 'Eurocash rusza z Innowacyjną Platformą Handlu - Handel dystrybucja.html',
+    'with': ['To dla nas ważna chwila', 'Jest to zintegrowany system', 'Jedną z najważniejszych', 'kasowy IPH POS.', 'dostęp do IPH za 1 zł.'],
+    'without': ['Szukasz lokalu handlowego', 'fot. shutterstock', 'Autor:', 'Grupa Eurocash wdraża Innowacyjną', 'Materiał chroniony prawem', 'więcej informacji:'],
+},
+'https://wiadomosci.gazeta.pl/wiadomosci/7,114883,27025667,ziemniaki-na-szostej-surowka-na-dziesiatej-jak-pomoc-zeby.html#s=BoxMMt2': {
+    'file': '_Ziemniaki na szóstej, surówka na dziesiątej_. Jak pomagać, żeby nie zaszkodzić_ [PORADNIK W PIGUŁCE].html',
+    'with': ['pomóc osobie niewidomej', 'Osoby niewidome, słabowidzące:', 'Gdy spotkasz osobę niewidomą', 'może wypaść z wózka.'],
+    'without': ['red.', 'Agencja Gazeta', 'cocopanda.pl', 'Komentarze'],
+},
+'https://www.radiomaryja.pl/informacje/sprzeciw-wobec-atakow-na-fundacje-lux-veritatis/': {
+    'file': 'Sprzeciw wobec ataków na Fundację Lux Veritatis – RadioMaryja.pl.html',
+    'with': ['Kolejne organizacje sprzeciwiają', 'Watchdog wytoczył', 'Nie możemy pozwolić na', 'nękania Fundacji Lux Veritatis'],
+    'without': ['Informacje, Polska', 'więcej]', 'Radio Maryja', 'drukuj', 'RIRM'],
+},
+'https://www.wsieciprawdy.pl/sieci-ipn-czyli-element-infrastruktury-krytycznej-pnews-4715.html': {
+    'file': '„Sieci”_ IPN, czyli element infrastruktury krytycznej Tygodnik Sieci.html',
+    'with': ['Zadania Instytutu Pamięci', 'co zechcą - czytamy.', 'Historyk zaznacza, że', 'łamach nowego wydania'],
+    'without': ['opublikowano:', 'Więcej o współcześnie', 'Zobacz także', 'Zapraszamy też do subskrypcji'],
+},
+'https://wpolityce.pl/gospodarka/549052-jest-zgoda-pe-na-umowe-handlowa-miedzy-ue-a-londynem': {
+    'file': 'Jest zgoda PE na umowę handlową między UE a Londynem.html',
+    'with': ['Zgoda PE jest', 'Parlament Europejski poparł', 'partnerem Unii Europejskiej', 'grudnia 2020 roku.', 'PE w odrębnym głosowaniu', 'współpracy z W.'],
+    'without': ['autor: Pixabay', 'opublikowano', 'I warmly welcome', 'aja/PAP', 'Czekamy na Wasze'],
+},
+'https://www.osw.waw.pl/pl/publikacje/analizy/2021-04-20/rosyjskie-zamachy-w-czechach-kontekst-krajowy-implikacje-perspektywy': {
+    'file': 'Rosyjskie zamachy w Czechach – kontekst krajowy, implikacje, perspektywy.html',
+    'with': ['17 kwietnia na konferencji', 'dopiero na 25 kwietnia.', 'Informacja o noszącym', 'Media czeskie i brytyjskie', 'w interesy Federacji Rosyjskiej.'],
+    'without': ['DO WYSŁUCHANIA W SERWISIE', 'Mateusz Seroka', 'NAJCZĘŚCIEJ CZYTANE', 'WIĘCEJ NA TEN TEMAT', 'Publikacje'],
+},
+'https://www.wirtualnemedia.pl/artykul/maciej-krzysztoszek-rzecznik-prasowy-amica-menedzer-komunikacji-zewnetrznej': {
+    'file': 'Maciej Krzysztoszek rzecznik prasowy Amica menedżer komunikacji zewnętrznej.html',
+    'with': ['Krzysztoszek odpowiada w', 'w dziale marketingu.', 'Wcześniej, przez blisko', 'Mickiewicza w Poznaniu.', 'i komunikacją wewnętrzną.'],
+    'without': ['Zmiany personalne', 'polecamy', 'autor:', 'komentarze', 'Podziel się', 'Wirtualnemedia.pl'],
+},
+'https://menway.interia.pl/historia/news-angus-barbieri-nie-jadl-przez-382-dni,nId,5222535#iwa_source=worthsee': {
+    'file': 'Angus Barbieri. Nie jadł przez 382 dni - Menway w INTERIA.PL.html',
+    'with': ['Angus pochodził ze', 'nie dłuższy niż 40 dni.', 'Początkowo Barbieri znajdował', 'cukru oraz mleka.'],
+    'without': ['domena publiczna', 'Opuszczony Dom', 'Szwedzka Dotacja', 'REKLAMA', 'STRONA'],
+},
+'https://energetyka24.com/rosjanie-sugeruja-natychmiastowe-odciecie-polski-od-gazu': {
+    'file': 'Rosjanie sugerują natychmiastowe odcięcie Polski od gazu - Energetyka24.html',
+    'with': ['Rosjanie twierdzą, że z', 'każdym razie nasz', 'Danię do Polski.', 'konkluduje finobzor.ru.'],
+    'without': ['DOTYCZY:', 'ZOBACZ TAKŻE', 'Reklama', 'KOMENTARZE', 'Prosimy o zaznaczenie', 'Czyżewski'],
+},
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# '': {
+#     'file': '.html',
+#     'with': [''],
+#     'without': [''],
+# },
+# 'https://www.msn.com/en-gb/news/uknews/uk-university-student-halls-too-full-to-be-safe-experts-warn/ar-BB19DUqK': {
+#     'file': 'msn.com.university.html',  # stays empty until download
+#     'author': 'Anna Fazackerley',
+#     'title': 'UK university student halls too full to be safe, experts warn ',
+#     'date': '2020-10-02',
+#     'description': 'As increasing numbers of UK universities wrestle with Covid-19 outbreaks, experts are warning that student halls are too full to be safe.&amp;nbsp;',  # in HTML source
+#     'categories': [''],
+#     'tags': [''],
+#     'with': ['s increasing numbers of UK universities wrestle with', 'seven days. In Northern Ireland, call your GP.', 'Some UK universities, including Cambridge, Imperial College London'],  # 3 segments
+#     'without': ['powered by Microsoft News', 'Sign in', 'Commenting is not currently available, but don’t worry'],  # 3 segments
+#     'comments': [''],  # 0 or 3 segments
+#     'license': '',  # if CC-...
+#     'region': 'UK',  # if obvious: DE, CH, AT
+# },
+}
